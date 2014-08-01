6ebb119e337b66a29ef699264d02f29464213d6f
Saleh Albatati <0xack13@gmail.com>
2014-08-01 05:40:26 +0300

    Add upload functionality

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 import flask, flask.views
 import os
 import functools
+from flask import Flask, request, redirect, url_for
+from werkzeug.utils import secure_filename
+from werkzeug import SharedDataMiddleware
 
-app = flask.Flask(__name__)
+
+UPLOAD_FOLDER = 'static/mp3/'
+ALLOWED_EXTENSIONS = set(['mp3'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 app.secret_key = "bacon"
 users = {'0xack13': 'password'}
@@ -55,6 +63,45 @@ class Player(flask.views.MethodView):
     def get(self):
         tracks = os.listdir('static/mp3/')
         return flask.render_template('player.html', tracks=tracks)
+    def upload_file():
+        if request.method == 'POST':
+            file = request.files['file']
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                return redirect(url_for('uploaded_file',
+                                        filename=filename))
+        return '''
+        <!doctype html>
+        <title>Upload new File</title>
+        <h1>Upload new File</h1>
+        <form action="" method=post enctype=multipart/form-data>
+          <p><input type=file name=file>
+             <input type=submit value=Upload>
+        </form>
+        '''
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('player'))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
 
 app.add_url_rule('/',
                  view_func=Main.as_view('index'),
@@ -64,8 +111,13 @@ app.add_url_rule('/home/',
                  methods=['GET', 'POST'])
 app.add_url_rule('/player/',
                  view_func=Player.as_view('player'),
-                 methods=['GET'])
+                 methods=['GET', 'POST'])
 
+app.add_url_rule('/uploads/<filename>', 'uploaded_file',
+                 build_only=True)
+app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
+    '/uploads':  app.config['UPLOAD_FOLDER']
+})
 
 app.debug = True
 app.run()
